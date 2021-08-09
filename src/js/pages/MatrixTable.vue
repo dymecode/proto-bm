31db9f9a8f0ae55d2438911e886cf7e69929f03e
Darren Ee <darren.ee@dymecode.com>
2021-08-09 20:53:16 +0800

    change list var to an object array instead of a flat array

--- src/js/pages/MatrixTable.vue
+++ src/js/pages/MatrixTable.vue
@@ -1,48 +1,52 @@
 <template>
-  <section class="section is-light matrix-table">
-    <div class="table-container">
-      <table class="table is-bordered">
-        <thead>
-          <tr>
-            <th></th>
-            <template v-for="(col, index) in regions">
-              <th :key="index">{{ col }}</th>
-            </template>
-          </tr>
-        </thead>
-        <tbody>
-          <template v-for="(row, i) in regions">
-            <tr :key="i">
-              <th>{{ row }}</th>
-              <template v-for="(rowcol, j) in regions">
-                <td :key="j">
-                  <div class="void-cell" v-if="rowcol === row"></div>
-                  <div class="empty-cell has-background-light" v-else>
-                    <button
-                      class="button is-light is-small"
-                      @click="reveal(row, rowcol)"
-                      v-if="j < i"
-                    >
-                      <i class="fas fa-plus"></i>
-                    </button>
-                    <button
-                      class="button is-light is-small"
-                      @click="reveal(rowcol, row)"
-                      v-if="j > i"
-                    >
-                      <i class="fas fa-plus"></i>
-                    </button>
-                  </div>
-                </td>
+  <div class="columns is-light matrix-table">
+    <div class="column is-8">
+      <div class="table-container">
+        <table class="table is-bordered">
+          <thead>
+            <tr>
+              <th></th>
+              <template v-for="col in regions">
+                <th :key="col.id">{{ col.name }}</th>
               </template>
             </tr>
-          </template>
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            <template v-for="(row, i) in regions">
+              <tr :key="i">
+                <th>{{ row.name }}</th>
+                <template v-for="(rowcol, j) in regions">
+                  <td :key="j">
+                    <div class="void-cell" v-if="rowcol.id === row.id"></div>
+                    <div class="empty-cell has-background-light" v-else>
+                      <button
+                        class="button is-light is-small"
+                        @click="reveal(row, rowcol)"
+                        v-if="j < i"
+                      >
+                        <i class="fas fa-plus"></i>
+                      </button>
+                      <button
+                        class="button is-light is-small"
+                        @click="reveal(rowcol, row)"
+                        v-if="j > i"
+                      >
+                        <i class="fas fa-plus"></i>
+                      </button>
+                    </div>
+                  </td>
+                </template>
+              </tr>
+            </template>
+          </tbody>
+        </table>
+      </div>
     </div>
-    <span class="title">{{ cross }}</span>
-
-  </section>
+    <div class="column is-4">
+      <p class="title">{{ crossName }}</p>
+      <p class="subtitle">{{ crossId }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -50,23 +54,25 @@
     name: "MatrixTable",
     data() {
       return {
-        cross: "",
+        crossName: "",
+        crossId: "",
         regions: [
-          "Europe",
-          "North Africa",
-          "North America",
-          "North Asia",
-          "Oceania",
-          "South Africa",
-          "South America",
-          "South East Asia",
-          "South Asia"
+          { id: 1, name: "Europe" },
+          { id: 2, name: "North Africa" },
+          { id: 3, name: "North America" },
+          { id: 4, name: "North Asia" },
+          { id: 5, name: "Oceania" },
+          { id: 6, name: "South Africa" },
+          { id: 7, name: "South America" },
+          { id: 8, name: "South East Asia" },
+          { id: 9, name: "South Asia" }
         ]
       };
     },
     methods: {
       reveal(x, y) {
-        this.cross = x + "/" + y;
+        this.crossName = x.name + "/" + y.name;
+        this.crossId = x.id + "-" + y.id;
         this.showModal();
       }
     }
@@ -76,6 +82,10 @@
 <style lang="scss">
   @use "../../sass/variables" as var;
   .matrix-table {
+    padding: 3rem 1rem;
+    .table-container {
+      overflow: auto;
+      scrollbar-width: thin;
     .table {
       table-layout: fixed;
 
@@ -85,8 +95,6 @@
           height: 4rem;
           font-size: 0.75rem;
           line-height: 1.2;
-          vertical-align: bottom;
-          text-align: center;
         }
       }
       tbody {
@@ -95,11 +103,9 @@
           height: 4rem;
           font-size: 0.75rem;
           line-height: 1.2;
-          vertical-align: middle;
-          text-align: right;
         }
         td {
-          width: 4rem;
+          width: 6rem;
           height: 4rem;
           font-size: 0.75rem;
           line-height: 1.2;
@@ -121,6 +127,7 @@
           }
         }
       }
+    }
     }
   }
 </style>
